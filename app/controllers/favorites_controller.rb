921eeb216dc94ae819dd301e87cca95b5ddcf938
otchi012 <hiroshi012931@gmail.com>
2020-11-01 13:41:33 +0000

    [Update]応援課題３ モデルとコントローラーを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,8 +5,11 @@ class FavoritesController < ApplicationController
     favorite.save
     redirect_to book_id(book)
   end
-  
+
   def destroy
-  
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to book_path(book)
   end
 end
